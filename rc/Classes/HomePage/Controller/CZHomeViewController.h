9b697eebd5802f18ef6c3bb9869e9e3f4caafad3
Alan-PC <121213072@qq.com>
2016-02-22 20:28:55 +0800

    02/22

--- rc/Classes/HomePage/Controller/CZHomeViewController.h
+++ rc/Classes/HomePage/Controller/CZHomeViewController.h
@@ -11,7 +11,7 @@
 @interface CZHomeViewController : UIViewController <UITableViewDelegate, UITableViewDelegate>
 
 @property (strong, nonatomic)  UIView *searchView;
-//@property (nonatomic, strong) UITableView *tableView;
+
 @property (strong, nonatomic)  UITableView *tableView;
 
 @end
