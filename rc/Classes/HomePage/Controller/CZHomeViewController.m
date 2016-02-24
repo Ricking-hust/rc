912086950f4881276ca38563833d30d01ad61cb9
余笃 <feave@icloud.com>
2016-02-24 19:32:00 +0800

    delete

--- rc/Classes/HomePage/Controller/CZHomeViewController.m
+++ rc/Classes/HomePage/Controller/CZHomeViewController.m
@@ -47,11 +47,7 @@
     
     [self.tableView reloadData];
 }
-- (void)viewWillAppear:(BOOL)animated
-{
-    
-    self.tableView.backgroundColor = [UIColor colorWithRed:245.0/255.0 green:245.0/255.0  blue:245.0/255.0  alpha:1.0];
-}
+
 #pragma mark - viewDidLoad
 - (void)viewDidLoad
 {
