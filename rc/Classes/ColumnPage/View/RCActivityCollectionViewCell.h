f1fc0dc6c98772c90b107542b5930581eda597ed
余笃 <feave@icloud.com>
2016-03-05 18:37:48 +0800

    重写专栏，删除scrollView部分

---
+++ rc/Classes/ColumnPage/View/RCActivityCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  activityCollectionViewCell.h
+//  rc
+//
+//  Created by 余笃 on 16/3/5.
+//  Copyright © 2016年 AlanZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCActivityCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic,strong) UIImageView *goodImageView;
+@property (nonatomic,strong) UILabel *goodNameLabel;
+
+@end
