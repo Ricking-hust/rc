3ce90e086c439b714538d883061c6ce00303f680
Alan-PC <121213072@qq.com>
2016-03-15 19:09:58 +0800

    2012

--- rc/Classes/ColumnPage/View/CZRightTableViewDelegate.m
+++ rc/Classes/ColumnPage/View/CZRightTableViewDelegate.m
@@ -9,6 +9,7 @@
 #import "CZRightTableViewDelegate.h"
 #import "CZTableView.h"
 #import "CZColumnCell.h"
+#import "ActivityModel.h"
 @implementation CZRightTableViewDelegate
 - (id)init
 {
@@ -29,50 +30,51 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    
     return self.array.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [[UITableViewCell alloc]init];
-    NSString *str = [NSString stringWithFormat:@"right %ld",indexPath.row];
-    cell.textLabel.text = str;
-    return cell;
-//    //1 创建可重用的自定义cell
-//    static NSString *reuseId = @"columnCell";
+
+    //1 创建可重用的自定义cell
+   // static NSString *reuseId = @"columnCell";
 //    CZColumnCell * cell = (CZColumnCell*)[tableView dequeueReusableCellWithIdentifier:reuseId];
 //    if (!cell)
 //    {
 //        cell = [[CZColumnCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseId];
 //    }
-//    cell.selectionStyle = UITableViewCellSelectionStyleNone;    //清除cell的点击状态
-//    cell.isLeft = NO;
-//    //对cell内的控件进行赋值
-//    
-//    
-//    //对cell内的控件进行布局
-//    [cell setSubviewConstraint];
-//    
-//    //2 返回cell
-//    return cell;
+    CZColumnCell * cell = [[CZColumnCell alloc]init];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;    //清除cell的点击状态
+    cell.isLeft = NO;
+    cell.backgroundColor = [UIColor clearColor];
+    //对cell内的控件进行赋值
+    [self setCellValue:cell AtIndexPath:indexPath];
+    
+    //对cell内的控件进行布局
+    [cell setSubviewConstraint];
+    
+    //2 返回cell
+    return cell;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+
     if (self.array.count-1 == indexPath.row &&self.subHeight != 0)
     {
         return self.subHeight;
     }
-    return 100;
-    //return 100;
-//    CZColumnCell *cell = (CZColumnCell *)[self tableView:tableView cellForRowAtIndexPath:indexPath];
-//    return cell.cellHeight;
+    CZColumnCell *cell = (CZColumnCell *)[self tableView:tableView cellForRowAtIndexPath:indexPath];
+    return cell.cellHeight;
 }
 //给单元格进行赋值
 - (void) setCellValue:(CZColumnCell *)cell AtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    ActivityModel *model = self.array[indexPath.row];
+    [cell.acImageView sd_setImageWithURL:[NSURL URLWithString:model.acPoster] placeholderImage:[UIImage imageNamed:@"20160102.png"]];
+    cell.acNameLabel.text = model.acTitle;
+    cell.acTimeLabel.text = model.acTime;
+    cell.acTagLabel.text = model.acTime;
 }
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
